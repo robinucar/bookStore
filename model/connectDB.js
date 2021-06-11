7b330fb616468b85c8884325b9f32d5b2cdd808e
robinucar <mehmet.ucar@imediapro.co.uk>
2021-06-11 17:29:54 +0100

    fixed moongose warning

--- model/connectDB.js
+++ model/connectDB.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     console.log('Successfully connected to DB');
   } catch (error) {
