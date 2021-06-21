ee66fbc8a8c825ca42acba5e8786514356a15f7a
robinucar <mehmet.ucar@imediapro.co.uk>
2021-06-21 20:37:15 +0100

    changed status code to unauthorized

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,13 +5,13 @@ const authMiddleware = (req, res, next) => {
   const token = req.header('token');
   //return error if token does not exist
   if (!token) {
-    return res.status(400).json({ msg: 'No token' });
+    return res.status(401).json({ msg: 'No token' });
   }
   //verify token
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
     console.log('decodedToken', decodedToken);
     if (err) {
-      return res.status(400).json({ msg: 'Invalid token' });
+      return res.status(401).json({ msg: 'Invalid token' });
     } else {
       req.decodedUser = decodedToken.userData;
       next();
